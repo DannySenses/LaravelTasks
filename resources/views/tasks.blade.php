db454e4ad78b62bb3eadf2c62ace1dc9e6d58e2c
DannySenses <danny@senses.co.uk>
2021-04-15 00:04:49 +0100

    modified views

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -6,21 +6,41 @@
 
     @include( "includes.create-task-form" )
 
-    <div class="mt-1 rounded-md shadow shadow-sm p-4 bg-white my-2 task-list">
+    <div class="mt-1 rounded-md shadow shadow-sm p-4 bg-white my-2 task-list incomplete-tasks">
 
         <h2 class="text-2xl font-semibold">In Progress</h2>
 
         <hr class="border-blue-400 my-1">
 
-        @foreach( $incomplete_tasks as $incomplete_task )
+        @if( $incomplete_tasks->count() > 0 )
 
-            <div class="task" id="task-{{ $incomplete_task->id }}">
+            @foreach( $incomplete_tasks as $incomplete_task )
 
-                <h3>{{ $incomplete_task->description }}</h3>
+                <div class="task" id="task-{{ $incomplete_task->id }}">
+
+                    <h3>{{ $incomplete_task->description }}</h3>
+
+                </div>
+
+            @endforeach
+
+        @else
+
+            <div class="text-center px-8 py-5">
+
+                <p><strong>You've got no tasks in progress, go you! Add a new task - or take the rest of the day off!</strong></p>
 
             </div>
 
-        @endforeach
+        @endif
+
+    </div>
+
+    <div class="mt-1 rounded-md shadow shadow-sm p-4 bg-white my-2 task-list completed-tasks">
+
+        <h2 class="text-2xl font-semibold">Completed Tasks</h2>
+
+        <hr class="border-blue-400 my-1">
 
     </div>
 
